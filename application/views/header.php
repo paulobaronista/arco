9b33d5d1fdbf8b904446d09a2ffffec76921727f
Paulo Baronista <paulobaronista@gmail.com>
2018-04-05 17:21:42 -0300

    Save projoeto

---
+++ application/views/header.php
@@ -0,0 +1,39 @@
+<!-- header section -->
+<header class="top-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-3 header-logo hidden-xs">
+                <a href="#home"><img src="<?= base_url(); ?>assets/images/logo.png" alt="" class="img-responsive logo"></a>
+            </div>
+
+            <div class="col-md-9">
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid nav-bar">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand visible-xs"  href="#home"><img src="<?= base_url(); ?>assets/images/logo.png" alt="" class="img-responsive logo_mobile"></a>
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a class="menu active" href="#home" >Home</a></li>
+                                <li><a class="menu" href="#servicos">Áreas de atuação</a></li>
+                                <li><a class="menu" href="#dentista-na-escola">Dentista na escola</a></li>
+                                <li><a class="menu" href="#primeiro-dia-no-dentista">Primeiro dia no dentista</a></li>
+                                <li><a class="menu" href="#harmonizacao-facial">Harmonização facial</a></li>
+                                <li><a class="menu" href="#contact">Contato</a></li>
+                            </ul>
+                        </div><!-- /navbar-collapse -->
+                    </div><!-- / .container-fluid -->
+                </nav>
+            </div>
+        </div>
+    </div>
+</header> <!-- end of header area -->
